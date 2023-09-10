a04ca3d904675e7597127ed8a54f581967c41723
sponkurtus2 <sponkurtus3@gmai.com>
2023-09-10 09:00:43 -0600

    Se construyo la app x7

--- Reporte/src/App.jsx
+++ Reporte/src/App.jsx
@@ -140,7 +140,7 @@ function Hhome() {
                   }}
                 />
                 <Avatar
-                  src="https://files.worldwildlife.org/wwfcmsprod/images/HERO_Penguins_Antarctica/story_full_width/9de57cats0_Medium_WW267491.jpg"
+                  src="./assets/A.png"
                   sx={{
                     "--Avatar-size": "180px",
                     margin: "0 10px", // Espacio horizontal entre las imágenes
