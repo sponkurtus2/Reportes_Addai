6c1e08b93798e5e3be4ffc844295617c90d2d80d
sponkurtus2 <sponkurtus3@gmai.com>
2023-09-10 09:21:46 -0600

    Subir a github

--- Reporte/vite.config.js
+++ Reporte/vite.config.js
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Reporte/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][ext]",
-      },
-    },
-  },
 });
